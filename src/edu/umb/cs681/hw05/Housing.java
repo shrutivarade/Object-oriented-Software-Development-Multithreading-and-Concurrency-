0d4628d028971fe6ab1eb08687a99247a33807fa
Shruti Varade <shrutivarade27@gmail.com>
2023-05-07 15:22:39 -0400

    HW05 commit code file

--- src/edu/umb/cs681/hw05/Housing.java
+++ src/edu/umb/cs681/hw05/Housing.java
@@ -44,12 +44,13 @@ public class Housing {
         Thread t4 = new Thread(dp4);
 
         t1.start();
-        t1.join();
         t2.start();
-        t2.join();
         t3.start();
-        t3.join();
         t4.start();
+
+        t1.join();
+        t2.join();
+        t3.join();
         t4.join();
 
         /*try{
